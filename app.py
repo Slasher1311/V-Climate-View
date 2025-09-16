3b80ea1da3df429cf9b81d2ef86490207bfca81d
Slasher1311 <nguyentiendoan76@gmail.com>
2025-09-16 16:48:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import pandas as pd
 from pmdarima import auto_arima
 from datetime import timedelta
-import os
 
 app = Flask(__name__)
 
@@ -164,5 +163,4 @@ def index():
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
